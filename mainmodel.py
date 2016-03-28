976bec053177fc6decaff3b1baf6873f80d8407e
Aditya Thyagarajan <aditya1503@gmail.com>
2016-03-28 15:16:50 +0530

    cn

--- mainmodel.py
+++ mainmodel.py
@@ -1,5 +1,59 @@
 from SiameseLSTM import *
 
+def train_lstm(train,max_epochs):
+    print "Training"
+    crer=[]
+    cr=1.6
+    freq=0
+    batchsize=32
+    dfreq=40#display frequency
+    valfreq=800# Validation frequency
+    lrate=0.0001
+    precision=2
+    for eidx in xrange(0,max_epochs):
+        sta=time.time()
+        num=len(train)
+        nd=eidx
+        sta=time.time()
+        print 'Epoch',eidx
+        rnd=sample(xrange(len(train)),len(train))
+        for i in range(0,num,batchsize):
+            q=[]
+            x=i+batchsize
+            if x>num:
+                x=num
+            for z in range(i,x):
+                q.append(train[rnd[z]])
+            #q=train[i:i+32]
+            #shuffle(q)
+            x1,mas1,x2,mas2,y2=prepare_data(q)
+            
+            ls=[]
+            ls2=[]
+            freq+=1
+            use_noise.set_value(1.)
+            for j in range(0,len(x1)):
+                ls.append(embed(x1[j]))
+                ls2.append(embed(x2[j]))
+            trconv=np.dstack(ls)
+            trconv2=np.dstack(ls2)
+            emb2=np.swapaxes(trconv2,1,2)
+            emb1=np.swapaxes(trconv,1,2)
+            cst=f_grad_shared(emb2, mas2, emb1,mas1,y2)
+            s=f_update(lrate)
+            ls=[]
+            ls2=[]
+            freq+=1
+            use_noise.set_value(1.)
+            
+            #s=f_update(lrate)
+            if np.mod(freq,dfreq)==0:
+                print 'Epoch ', eidx, 'Update ', freq, 'Cost ', cst
+        sto=time.time()
+        print "epoch took:",sto-sta
+
+
+print training
 newp=creatrnnx()
 for i in newp.keys():
     if i[0]=='1':
@@ -30,6 +84,7 @@ ns=emb11.shape[1]
 f2sim=theano.function([emb11,mask11,emb21,mask21],sim,allow_input_downcast=True)
 f_cost=theano.function([emb11,mask11,emb21,mask21,y],cost,allow_input_downcast=True)
 if training==True:
+    print "Training is True"
     gradi = tensor.grad(cost, wrt=tnewp.values())#/bts
     grads=[]
     l=len(gradi)
@@ -40,15 +95,12 @@ if training==True:
     for i in range(0,len(tnewp.keys())/2):
         grads.append(grads[i])
 
-
-
-
-
-    f_grad_shared, f_update = adadelta(lr, tnewp, grads,
-                                       emb11,mask11,emb21,mask21,y, cost)
+    f_grad_shared, f_update = adadelta(lr, tnewp, grads,emb11,mask11,emb21,mask21,y, cost)
 
 
 train=pickle.load(open("stsallrmf.p","rb"))#[:-8]
 t2=train
 print "Pre-training"
 train_lstm(train,66)#46 DONE
+test=pickle.load(open("semtest.p",'rb'))
+print chkterr2(test)
